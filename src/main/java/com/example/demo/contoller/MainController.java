738681f869b852bff0154f70c71da2ff887867c2
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-03 11:23:59 -0500

    initial commit

---
+++ src/main/java/com/example/demo/contoller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.demo.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String helloSecurity() {
+        return "Hello World! - Secured";
+    }
+}
